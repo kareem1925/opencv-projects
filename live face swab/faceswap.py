265a3a5afa14c1efb9011ac217aca661f76c28d0
Kareem EL-Safty <30449261+kareem1925@users.noreply.github.com>
2018-05-12 21:27:38 +0200

    Update faceswap.py

--- live face swab/faceswap.py
+++ live face swab/faceswap.py
@@ -213,7 +213,7 @@ cap = cv2.VideoCapture(0)
 # dlibOn controls if use dlib's facial landmark detector (better) 
 # or use HAAR Cascade Classifiers (faster)
 
-filter_image =  "images/Trump.jpg" ### Put your image here!
+filter_image =  "Trump.jpg" ### Put your image here!
 dlibOn = True
 
 while True:   
